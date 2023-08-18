fc8023af64d60d2caf8bab94beea4eaef976da6c
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-08-18 18:33:41 -0500

    [client] feat: Enable https on vite dev server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "vite";
+import fs from "fs";
 import path from "path";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+const httpsConfig = {
+  cert: "C:/ssl/certificate_data/__zyae_net.crt",
+  key: "C:/ssl/certificate_data/__zyae_net.key",
+};
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "zyae.net",
+    port: 3000,
+    https: {
+      key: fs.readFileSync(httpsConfig.key),
+      cert: fs.readFileSync(httpsConfig.cert),
+    },
+    open: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
