1e3e58700b1bdfedd0a5cf42b9560280a5ddaa90
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-08-18 18:43:01 -0500

    [client] feat: Add token service hook

---
+++ client/src/hooks/useTokenService.ts
@@ -0,0 +1,49 @@
+import { clearUser, setUser } from "@/stores";
+import { useEffect, useState } from "react";
+
+import { User } from "@/types";
+import { axios } from "@/libs";
+import { useAppDispatch } from ".";
+
+const getMe = async () => {
+  try {
+    await getNewTokens();
+    const user = (await axios.get<{ data: { user: User } }>("db/users/me")).data
+      .data.user;
+    return user;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+const getNewTokens = async () => {
+  try {
+    await axios.get("/auth/sessions/tokens");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const useTokenService = () => {
+  const dispatch = useAppDispatch();
+
+  const [status, setStatus] = useState<"loading" | "auth" | "noauth">(
+    "loading"
+  );
+
+  useEffect(() => {
+    getMe()
+      .then((user) => (dispatch(setUser(user)), setStatus("auth")))
+      .catch(() => (dispatch(clearUser()), setStatus("noauth")));
+
+    const refreshInterval = setInterval(
+      async () => await getNewTokens(),
+      1000 * 60 * 5
+    );
+
+    return () => clearInterval(refreshInterval);
+  });
+
+  return { status };
+};
--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,7 +1,10 @@
 import { publicRoutes } from "./publicRoutes";
 import { useRoutes } from "react-router-dom";
+import { useTokenService } from "@/hooks";
 
 export const AppRoutes = () => {
+  const { status } = useTokenService();
+
   const element = useRoutes(publicRoutes);
 
   return element;
