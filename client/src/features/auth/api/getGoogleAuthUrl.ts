f8221f934b74db3adda720f160df4ee6297a8215
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-08-25 00:41:34 -0500

    [client] feat: Add rd param in getGoogleAuthUrl

--- client/src/features/auth/api/getGoogleAuthUrl.ts
+++ client/src/features/auth/api/getGoogleAuthUrl.ts
@@ -1,9 +1,9 @@
 import { axios } from "@/libs/axios";
 
-export const getGoogleAuthUrl = async (): Promise<string> => {
+export const getGoogleAuthUrl = async (rd: string | null): Promise<string> => {
   return (
     await axios.get<{ data: { url: string } }>(
-      `/auth/google/url/?rd=${document.location.origin}`
+      `/auth/google/url/?rd=${rd || document.location.origin}`
     )
   ).data.data.url;
 };
