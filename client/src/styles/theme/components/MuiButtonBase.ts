e2eba33f5cad262180230db06ae9443535b696c3
davidbzyae <102764748+davidbzyae@users.noreply.github.com>
2023-09-02 00:12:29 -0500

    [client] feat: Add blur to touch ripple

--- client/src/styles/theme/components/MuiButtonBase.ts
+++ client/src/styles/theme/components/MuiButtonBase.ts
@@ -1,3 +1,11 @@
 import { Components, Theme } from "@mui/material";
 
-export const MuiButtonBase: Components<Theme>["MuiButtonBase"] = {};
+export const MuiButtonBase: Components<Theme>["MuiButtonBase"] = {
+  styleOverrides: {
+    root: {
+      ".MuiTouchRipple-child": {
+        filter: "blur(12px) opacity(.5)",
+      },
+    },
+  },
+};
